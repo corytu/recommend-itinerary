4d3f30300990048f9f78ea7580130f06e2c6bd56
corytu <corytu@users.noreply.github.com>
2019-08-09 23:47:44 +0800

    Add encoding declaration and comments about env variables

--- import_articles.py
+++ import_articles.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding=utf-8
+
 # Import articles to Google Cloud Firestore
 
 import json
@@ -6,6 +9,9 @@ import hashlib
 from google.cloud import firestore
 
 def import_articles():
+    # Two environment variables are defined
+    # GOOGLE_APPLICATION_CREDENTIALS - json credentials for the service account
+    # GCLOUD_PROJECT - project ID
     db = firestore.Client()
     db_collection = db.collection("articles_182k")
     f = open("articles_182k")
