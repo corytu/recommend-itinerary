d5ff52fb9221eda487f0a51e8750111e4661545e
corytu <corytu@users.noreply.github.com>
2019-08-20 21:43:46 +0800

    POC of getting all documents by paginating data

---
+++ get_article_titles.py
@@ -0,0 +1,34 @@
+import os
+import json
+
+from google.cloud import firestore
+
+def get_article_titles():
+    if "title_url.json" in os.listdir():
+        with open("title_url.json") as f:
+            title_url_dic = json.load(f)
+    else:
+        db = firestore.Client()
+        article_ref = db.collection("articles_182k")
+        title_url_dic = {}
+        # Paginating data with query cursors: https://cloud.google.com/firestore/docs/query-data/query-cursors
+        # Also see https://stackoverflow.com/a/57526797/6666231
+        query = article_ref.order_by("url").limit(10000)
+        while True:
+            old_title_url_dic_len = len(title_url_dic)
+            docs = query.stream()
+            for doc in docs:
+                doc_dict = doc.to_dict()
+                title_url_dic[doc_dict["title"]] = doc_dict["url"]
+                last_url = doc_dict["url"]
+                print(f"Get article: {doc_dict['title']}")
+            new_title_url_dic_len = len(title_url_dic)
+            if new_title_url_dic_len > old_title_url_dic_len:
+                query = article_ref.order_by("url").start_after({"url": last_url}).limit(10000)
+            else:
+                with open("title_url.json", "w") as f:
+                    json.dump(title_url_dic, f, indent=2, ensure_ascii=False)
+                break
+
+if __name__ == "__main__":
+    get_article_titles()
